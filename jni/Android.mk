4a040989d25c18bdb535b6b7f8a040cb51de5174
pagxir <pagxir@gmail.com>
2013-01-23 11:39:24 +0800

    update Android.mk for use building android 2.3 system

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,8 +9,10 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libproxy5
 LOCAL_SRC_FILES := appface.c proxy5.cpp tcp_listen.cpp tcp_socks.cpp
-LOCAL_C_INCLUDES +=  $(JNI_H_INCLUDE) $(LOCAL_PATH)/../libwait/include
-LOCAL_CXX_INCLUDES +=  $(JNI_H_INCLUDE) $(LOCAL_PATH)/../libwait/include
+LOCAL_C_INCLUDES +=  $(JNI_H_INCLUDE) $(LOCAL_PATH)/libwait/include
+LOCAL_CXX_INCLUDES +=  $(JNI_H_INCLUDE) $(LOCAL_PATH)/libwait/include
 LOCAL_STATIC_LIBRARIES = libwait liblog
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+include $(call all-makefiles-under,$(LOCAL_PATH))

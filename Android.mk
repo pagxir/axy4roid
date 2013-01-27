2665373680037de96d0f040868787e16f5bdad89
pagxir <pagxir@gmail.com>
2013-01-27 17:04:53 +0800

    add youmi ad to this project

--- Android.mk
+++ Android.mk
@@ -4,8 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := Axy4roid
-LOCAL_CERTIFICATE := platform
-LOCAL_PACKAGE_NAME := prox4roid
 LOCAL_JNI_SHARED_LIBRARIES := libproxy5
 LOCAL_STATIC_JAVA_LIBRARIES := youmisdk
 
